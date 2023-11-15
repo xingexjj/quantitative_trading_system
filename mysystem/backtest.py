71b2841b5e27e7ddb39ab34e3658cba0cfb85c02
xingexjj <1900010601@pku.edu.cn>
2023-11-15 22:51:33 +0800

    update dataset type to pd.DataFrame

--- mysystem/backtest.py
+++ mysystem/backtest.py
@@ -3,5 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class Backtest:
-    def __init__(self, data, ):
-        pass
+    def __init__(self, ret: pd.DataFrame) -> None:
+        self.ret = ret
+
+    def backtest(self, alpha, start, end, init_cap = 1e8, pool = 'all', ic = True, pnl = True):
+        alpha = alpha.shift(1)
+        weight = self.get_weight(alpha)
+
+    def get_weight(self, alpha: pd.DataFrame) -> pd.DataFrame:
+        return alpha / np.nansum(abs(alpha))
